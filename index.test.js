f97176329cd13ad270c457efb0d7da71dc9f5d54
saraiesco <saraiescobar@google.com>
2024-01-30 13:23:13 -0600

    working post part 1

--- index.test.js
+++ index.test.js
@@ -33,4 +33,39 @@ describe('Endpoints', () => {
             expect(response.body[0]).toEqual(expect.objectContaining(dogs[0]));
         });
     });
+
+    describe('POST /dogs', () => {
+        it('should add a dog to the list of dog', async () => {
+            // make a request
+            // const dog = { breed: "Pug Mix", name: "Frodo", color: "beige", description: "Elder pug mix with a hunchback, snaggletooth, and scruffy face" };
+            const response = await request(app)
+                .post('/dogs')
+                //.send
+                .send( testDogData)
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                 // Assert (expect) that the data coming back matches the data in testDogData.
+                .then(response => {
+                    expect(response.body.name).toEqual(testDogData.name)
+                 });
+        });
+
+        it('query database for matching response.body.id', async () => {
+            // In another test (another it), query the database for a dog of id matching that of the response.body.id coming back from the API. Assert that the dog data from the db matches the request body.
+
+        });
+    });
+
+
+    // describe('DELETE /dogs/:id', () => {
+    //     it('should delete dog data of :id', async () => {
+    //         // This time, send a DELETE request to /dogs/1 via supertest.
+
+    //         // Query the database for a dog with ID of 1.
+
+    //         // Assert that the dog returned from the findAll is null. Use Jest’s toBeNull.
+       
+    //     });
+    // });
 });
